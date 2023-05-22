c569dba1c2ab07bcef376fd5ce07daa7bbc4665a
xardb <vlad96mir@bk.ru>
2023-05-22 20:36:08 +0300

    ура победа

---
+++ script.js
@@ -0,0 +1,24 @@
+// // let a = 5;
+// // let b = 10;
+// // let c = 8;
+
+// // function foo () {
+// //     const a = Number(prompt('sl1', 0));
+// //     const b = Number(prompt('sl2', 0));
+// //     if(Number.isNaN(a) && Number.isNaN(b)) {
+// //         alert('хуита');
+// //     }
+// //     else {
+// //         alert(a+b);
+// //     }
+// // }
+
+// // foo()
+
+// var arr = ["роутер", "палец", "мандарин", "бутылка",]
+
+// for(let i=0; i <= 3; i=i+1) {
+//     if(i%2===0){
+//     console.log(arr[i]);
+// }
+// }
